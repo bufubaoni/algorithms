a67a76641b9a18d6c194ae592add1ffc1f1f05aa
bufubaoni <bufubaoni@163.com>
2020-05-15 16:21:13 +0800

    update subarraySum

--- subarray-sum-equals-k.py
+++ subarray-sum-equals-k.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 class Solution(object):
     def subarraySum(self, nums, k):
         """
@@ -9,7 +9,7 @@ class Solution(object):
 
         cnt = 0
         su = 0
-        cul = {0:1}
+        cul = {0: 1}
         for item in nums:
             su += item
             if su - k in cul:
@@ -19,4 +19,30 @@ class Solution(object):
             cul[su] = re
         return cnt
 
-# cul 记录 所有数字的和， 如果 之后减去k 在cul 中说明 此时之和减去 之前的小线段会得到结果
+
+def get_sub(nums, k):
+    # 获取连续子数组 等于k
+    res = []
+
+    su = 0
+    cul = {0: [0]}
+
+    for idx, item in enumerate(nums):
+        su += item
+
+        if su - k in cul:
+            for start in cul[su-k]:
+                if (start, idx) not in res:
+                    res.append((start, idx))
+        re = cul.get(su, [])
+        re += [idx]
+        cul[su] = re
+    print cul
+    return [nums[start:end] for start, end in res]
+    # cul 记录 所有数字的和， 如果 之后减去k 在cul 中说明 此时之和减去 之前的小线段会得到结果
+
+
+if __name__ == "__main__":
+    print(get_sub([1, 1, 1], 2))
+
+    print([1, 1, 1][-1:2])
